2d8da7f6e5db0f89277b13519bb029a07d65c394
tpct <eslamjackie@outlook.com>
2025-03-07 08:25:52 +0200

    first commit

--- Runner.py
+++ Runner.py
@@ -1,11 +1,8 @@
 import time
 from threading import Thread
-from time import sleep
-
 from Core.Chatbot import Chatbot
 from Core.Account import Account
-from Core.PresenceSocket import PresenceSocket
-from Core.MessagingSocket import MessagingSocket
+
 
 OPEN_AI_SECRET = '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'
 OPEN_AI_MODEL_ID = 'ft:gpt-3.5-turbo-0125:personal::B7issDw8'
